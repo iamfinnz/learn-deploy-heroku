138cba0e8af9d286d3f4b840c973a303b713bc45
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2023-03-03 22:03:12 +0700

    update app

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return 'Ini tes aja'
+    
 @app.route("/hello")
 def index():
     return render_template("tes.html")
